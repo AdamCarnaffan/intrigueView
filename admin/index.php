c0b26e4ab6c3a17122ace14cd69be0504f1859b2
Adam Carnaffan <acarnaffan@hotmail.com>
2017-10-10 20:46:30 -0400

    Fixed Some Pathing
    Also started the tagging system

--- admin/index.php
+++ admin/index.php
@@ -28,7 +28,7 @@ include('validateUser.php');
 <body class="hide-overflow">
 
   <nav class="navbar navbar-expand-md navbar-dark bg-dark mb-4">
-    <a class="navbar-brand" href="#">IntrigueView</a>
+    <a class="navbar-brand" href="../index.php">IntrigueView</a>
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
--- index.php
+++ index.php
@@ -20,14 +20,14 @@
 <body class="hide-overflow" onresize='resizeCanvas'>
   <!-- Fixed navbar -->
 <nav class="navbar navbar-expand-md navbar-dark bg-dark mb-4">
-  <a class="navbar-brand" href="index.html">IntrigueView</a>
+  <a class="navbar-brand" href="index.php">IntrigueView</a>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
   </button>
   <div class="collapse navbar-collapse" id="navbarCollapse">
     <ul class="navbar-nav mr-auto">
       <li class="nav-item active">
-        <a class="nav-link" href="index.html">Home <span class="sr-only">(current)</span></a>
+        <a class="nav-link" href="index.php">Home <span class="sr-only">(current)</span></a>
       </li>
       <!-- USED TO BROWSE FEEDS
       <li class="nav-item active">
