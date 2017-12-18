1139724ad4ad4f9e43600b8d9be9ae96c291403e
Adam Carnaffan <acarnaffan@hotmail.com>
2017-12-17 23:01:00 -0500

    Plan Started for SmartFetch

---
+++ smartFetch.php
@@ -0,0 +1,24 @@
+<?php 
+
+/*
+
+Idea: The feature takes tag data submitted and uses the data to find related articles online through google news
+
+1) Fetch Global / Personal data (input)
+  -Get data and weight factors from database for incoming query
+  
+
+2) Apply Data to Google News Query (output)
+  -Take top recent results for the Query
+  -Check that the site of the result is not on the site blacklist (fake news filter)
+  -Process the entry through the usual entry processing method
+  -Submit the entry object for validation
+
+3) Check for match between fetched article and original data (validation)
+
+*/
+
+
+
+
+ ?>
