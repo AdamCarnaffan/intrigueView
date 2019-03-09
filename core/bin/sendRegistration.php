0fe51cd4c1a34ea99f3ba28abdb06930be8d9109
Divy <divyr@outlook.com>
2019-03-09 01:33:58 -0500

    Semi-Finished Registration Validation

--- core/bin/sendRegistration.php
+++ core/bin/sendRegistration.php
@@ -1,5 +1,7 @@
 <?php
-require_once('class/class_userData.php');
+require_once('../config.php');
+require_once(ROOT_PATH . '/class/class_userData.php');
+require('dbConnect.php');
 
 // NEEDS TO INCORPORATE PRELOGIN DATA
 
@@ -10,50 +12,6 @@ $email = $_POST['email'];
 register($username, $password, $email);
 
 function register($username, $password, $email) {
-  // Create Query
-  require('dbConnect.php');
-
-  // Username must be at least 5 characters
-  if (strlen($username) < 5) {
-    echo "Your Username is too short";
-    return;
-  }
-  // Username may not be more than 20 Characters
-  if (strlen($username) > 20) {
-    echo "Your Username is too long";
-    return;
-  }
-  // Username must not include any illegal characters
-  if (preg_match("~[\p{P}<>[^_]]~", $username)) {
-    echo "Your Username may not include any punctuation";
-    return;
-  }
-  // Username must include a letter of the greek alphabet
-  if (!preg_match("~[A-Za-z]~", $username)) {
-    echo "Your Username must include a letter";
-    return;
-  }
-  // Username must BEGIN with a letter
-  if (!preg_match("~^[A-Za-z]~", $username)) {
-    echo "Your Username must begin with a letter";
-    return;
-  }
-  // Password must be at least 8 characters
-  if (strlen($password) < 8) {
-    echo "Your Password must be at least 8 characters";
-    return;
-  }
-  // No email address is less than 2 characters
-  if (strlen($email) < 2) {
-    echo "Your email address is invalid";
-    return;
-  }
-  // a password needs at least 1 number and 1 capital or lowercase letter
-  if (!preg_match("~[0-9]~", $password) || !preg_match("~[a-z A-Z]~", $password)) {
-    echo "Your Password must contain letters and numbers";
-    return;
-  }
-
   // Build Query
 
   // Hash password
